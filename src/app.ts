a925e07ca6b371c8b58e80c88d0099c288bb22df
eric_espinoza <espinoza.eric@gmail.com>
2023-12-18 02:13:21 -0300

    configuration yargs

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,21 @@
-import fs from 'fs' // tabmien se puede desestructurar
-
-const number = 5
-
-const multiplicacion = () => {
-    
-
-    console.log(`
-        ==============================================
-        Tabla del ${number}
-        ==============================================
-
-        ${number} x 1 = ${number * 1}
-        ${number} x 2 = ${number * 2}
-        ${number} x 3 = ${number * 3}
-        ${number} x 4 = ${number * 4}
-        ${number} x 5 = ${number * 5}
-        ${number} x 6 = ${number * 6}
-        ${number} x 7 = ${number * 7}
-        ${number} x 8 = ${number * 8}
-        ${number} x 9 = ${number * 9}
-    `)
-}
-multiplicacion()
+import { yarg as argv } from './config/plugins/yargs.plugins'
 
 
-/*******************************************************************************/
-// Desarrollo esperado
-/*******************************************************************************/
-let outputMessage = '';
-const base = 6;
-const headerMessage = `
-==========================================
-        Tabla del ${ base }
-==========================================
-`
+(async() => {
+    await main();
+    console.log('Fin del programa')
+})()
 
-for (let i = 1; i <= 10; i++ ) {
-    outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
+async function main() {
+    //console.log(argv.b);
+    //console.log(argv.base);
+    console.log(argv);
 }
 
-
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage)
-
-
-
-const outputPath = `outputs`;
+/* const main = async () => {
+    return console.log('Main Ejecutando');
+}
+ */
 
 
 
-fs.mkdirSync(outputPath, { recursive: true });
-fs.writeFileSync(`${ outputPath }/tabla-${ base }.txt`, outputMessage);
-console.log('File created!');
