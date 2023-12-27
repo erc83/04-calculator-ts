b29b4a2ba892eb521c2f8487abe1ff21b828ffef
eric_espinoza <espinoza.eric@gmail.com>
2023-12-27 00:05:04 -0300

    test create table use case ok

--- src/domain/use-cases/create-table.use-case.ts
+++ src/domain/use-cases/create-table.use-case.ts
@@ -21,7 +21,9 @@ export class CreateTable implements CreateTableUseCase {
     execute({ base, limit = 10 } : CreateTableOptions){     //limite = 10 porque es opcional
         let outputMessage = '';
         for (let i = 1; i <= limit; i++ ) {
-            outputMessage += `${ base } x ${ i } = ${ base * i }\n`;
+            outputMessage += `${ base } x ${ i } = ${ base * i }`;
+
+            if( i < limit ) outputMessage += '\n';
         }
 
         return outputMessage;
