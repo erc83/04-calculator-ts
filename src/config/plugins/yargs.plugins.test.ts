2d409c37542c5872b0454648867241a96c92af72
eric_espinoza <espinoza.eric@gmail.com>
2023-12-28 01:11:47 -0300

    test argv valores personalizados

--- src/config/plugins/yargs.plugins.test.ts
+++ src/config/plugins/yargs.plugins.test.ts
@@ -4,19 +4,25 @@ const runCommand =  async(args: string[] ) => {    // es una funcion que permite
 
     process.argv = [ ...process.argv, ...args ]
 
-    const defaultExport = await import('./yargs.plugins')
+    // const defaultExport = await import('./yargs.plugins')
     const { yarg } = await import('./yargs.plugins')
 
     return yarg   // si colocamos elobjeto encima veremos el objeto de configuracion
 }
 
 describe('Test yargs.plugin.ts', () => {
+    const originalArgv = process.argv;
+    
+    beforeEach(() => {
+        process.argv = originalArgv;
+        jest.resetModules();
+    })
 
     test('should return default values', async ()=> {
         
         const argv = await runCommand(['-b', '5'])
 
-        console.log(argv)
+        //console.log(argv)
         //console.log(process.argv)
         //console.log(yarg)
 
@@ -32,4 +38,19 @@ describe('Test yargs.plugin.ts', () => {
 
     })
 
+    test('should return cofiguration with custom values ', async () => {
+        // cambiar la base
+        // cambiar el limite con el runCommand
+        const argv = await runCommand(['-b','5','-l','20', '-s','-n', 'custom-name','-d', 'custom-dir']);
+
+        // console.log(argv)
+        expect( argv ).toEqual(expect.objectContaining({
+            b: 5,
+            l: 20,
+            s: true,
+            n: 'custom-name',
+            d: 'custom-dir',
+          }))
+
+    })
 })
